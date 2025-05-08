8f7d8d455bc9b5b38c56a0b31cbc972c6f0a3e8a
yougeshshah <yougeshshah0@gmail.com>
2025-05-08 11:21:44 +0545

    first change

--- mudkhu_heritage/settings.py
+++ mudkhu_heritage/settings.py
@@ -72,14 +72,21 @@ WSGI_APPLICATION = 'mudkhu_heritage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('mudkhu_heritage  '),
+        'USER': os.getenv('mudkhu_user'),
+        'PASSWORD': os.getenv('Yoj44401'),
+        'HOST': os.getenv('localhost'),
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
